5ef22864c317a454cfd5d68c8fe477c45ae49df8
David Planella <david.planella@gmail.com>
2012-05-27 23:53:34 +0200

    Still modifying config to get readthedocs to build

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['dbus', 'dbus.mainloop.glib']
+MOCK_MODULES = ['dbus', 'dbus.mainloop.glib', 'dbus.mainloop', 'mainloop.glib']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
