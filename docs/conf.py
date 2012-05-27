0f07c5511bb33daf7293409b0a662ec30c20a720
David Planella <david.planella@gmail.com>
2012-05-27 23:33:33 +0200

    Modified Sphinx config file to work with readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,10 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, root_path)
+
+import Geoclue
 
 # -- General configuration -----------------------------------------------------
 
