8cf2a489178ff9c3c40b846a9a39f0bfc24959c5
David Planella <david.planella@gmail.com>
2012-06-01 00:30:34 +0300

    Update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ class Mock(object):
            return Mock()
 
 MOCK_MODULES = [
-    'dbus', 'dbus.mainloop', 'dbus.mainloop.glib',
+    'dbus', 'dbus.mainloop', 'dbus.mainloop.glib', 'DBusGMainLoop'
     ]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
