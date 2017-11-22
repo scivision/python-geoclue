7242f26c61322d12cb665bd0411f7ce8ddda6d40
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-11-21 19:34:10 -0500

    log output to disk

--- locWifi.py
+++ locWifi.py
@@ -2,29 +2,60 @@
 """
 get location using Skyhook Wifi < 100 m accuracy in city.
 """
+from time import sleep
 from datetime import datetime
 import Geoclue
 import Geoclue.geoclue
 
-
-# feel free to define your own, it's just .ini style files.
-geoloc = Geoclue.DiscoverLocation('/usr/share/geoclue-providers')
-
-# finer accuracy than LOCALITY causes Skyhook Ubuntu-geoip to not work.
-geoloc.init(accuracy=Geoclue.geoclue.ACCURACY_LEVEL_LOCALITY)
-#    geoloc.signal()
-
+def query_pos(geoloc):
 # %% MUST set provider or you'll get cached result until PC reboots!
-geoloc.set_position_provider('Skyhook')
+    geoloc.set_position_provider('Skyhook')
 # %% read location and print result
-loc = geoloc.get_location_info()
-if not loc:
-    raise RuntimeError("Could not find location. Internet connection is required!")
+    loc = geoloc.get_location_info()
+    if not loc:
+        raise RuntimeError("Could not find location. Internet connection is required!")
 
-lat = float(loc['latitude'])
-lon = float(loc['longitude'])
-tpos = datetime.fromtimestamp(int(loc['position_timestamp']))
+    return loc
 
-print("{}: timestamp / Lat / Lon  {}   {} {}".format(
-        geoloc.get_position_provider(),tpos,lat,lon))
+def print_loc(loc):
+    """prints location in single line"""
+
+    lat = float(loc['latitude'])
+    lon = float(loc['longitude'])
+    tpos = datetime.fromtimestamp(int(loc['position_timestamp']))
+
+    stat = "{} {} {} {}".format(
+        geoloc.get_position_provider(),tpos,lat,lon)
+
+    print(stat)
+
+    return stat
+
+if __name__ == '__main__':
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('logfile',help='logfile to append location to',nargs='?')
+    p = p.parse_args()
+
+    logfile = p.logfile
+
+# %% feel free to define your own, it's just .ini style files.
+    geoloc = Geoclue.DiscoverLocation('/usr/share/geoclue-providers')
+# %% finer accuracy than LOCALITY causes Skyhook Ubuntu-geoip to not work.
+    geoloc.init(accuracy=Geoclue.geoclue.ACCURACY_LEVEL_LOCALITY)
+
+    while True:
+        loc = query_pos(geoloc)
+        stat = print_loc(loc)
+
+        if logfile:
+            with open(logfile,'a') as f:
+                f.write(stat)
+
+        sleep(60) # fastest update rate with Skyhook is one minute
+
+
 
